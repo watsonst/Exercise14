4be2e19bab963b79c2e9f732f56660f2880e5e27
Stephanie <me@stephaniemariewatson.com>
2021-08-11 11:23:12 -0400

    first commit for exercise 14

---
+++ Exercise14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= input ; i++)
+                {
+                    int sqr = (int)Math.Pow(i, 2);
+                    Console.Write($"{sqr} ");
+                }
+
+                Console.Write("\nWould you like to continue? y/n");
+                answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    break;
+                }
+
+            }
+            while (answer == "y");
+        }
+
+    }
+}
